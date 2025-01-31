01624c3cd6bf732f3c0e42a190ebfcc2da3f0edf
Leg-10n <kevin.mon@student.mahidol.edu>
2025-01-31 18:43:49 +0700

    feat: added the search bar feature to the inventory page

--- iccs372proj1/inventory/urls.py
+++ iccs372proj1/inventory/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem
+from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem, SearchSuggestions
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+    path("search_suggestions/", SearchSuggestions.as_view(), name="search_suggestions"),
 ]
--- iccs372proj1/inventory/views.py
+++ iccs372proj1/inventory/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
@@ -5,77 +6,100 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, InventoryItemForm
 from .models import InventoryItem, Category
+
 from iccs372proj1.settings import LOW_QUANTITY
 from django.contrib import messages
 
 class Index(TemplateView):
-	template_name = 'inventory/index.html'
+    template_name = 'inventory/index.html'
 
 class Dashboard(LoginRequiredMixin, View):
-	def get(self, request):
-		items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
+    def get(self, request):
+        search_query = request.GET.get('q', '')  # Get the search query from the URL
+        items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
 
-		low_inventory = InventoryItem.objects.filter(
-			user=self.request.user.id,
-			quantity__lte=LOW_QUANTITY
-		)
+        if search_query:
+            items = items.filter(name__icontains=search_query)  # Filter items by search query
 
-		if low_inventory.count() > 0:
-			if low_inventory.count() > 1:
-				messages.error(request, f'{low_inventory.count()} items have low inventory')
-			else:
-				messages.error(request, f'{low_inventory.count()} item has low inventory')
+        low_inventory = InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lte=LOW_QUANTITY
+        )
 
-		low_inventory_ids = InventoryItem.objects.filter(
-			user=self.request.user.id,
-			quantity__lte=LOW_QUANTITY
-		).values_list('id', flat=True)
+        if low_inventory.count() > 0:
+            if low_inventory.count() > 1:
+                messages.error(request, f'{low_inventory.count()} items have low inventory')
+            else:
+                messages.error(request, f'{low_inventory.count()} item has low inventory')
 
-		return render(request, 'inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
+        low_inventory_ids = InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lte=LOW_QUANTITY
+        ).values_list('id', flat=True)
+
+        return render(
+            request,
+            'inventory/dashboard.html',
+            {
+                'items': items,
+                'low_inventory_ids': low_inventory_ids,
+                'search': search_query  # Pass the search query to the template
+            }
+        )
+
+
 
 class SignUpView(View):
-	def get(self, request):
-		form = UserRegisterForm()
-		return render(request, 'inventory/signup.html', {'form': form})
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form': form})
 
-	def post(self, request):
-		form = UserRegisterForm(request.POST)
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
 
-		if form.is_valid():
-			form.save()
-			user = authenticate(
-				username=form.cleaned_data['username'],
-				password=form.cleaned_data['password1']
-			)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
 
-			login(request, user)
-			return redirect('index')
+            login(request, user)
+            return redirect('index')
 
-		return render(request, 'inventory/signup.html', {'form': form})
+        return render(request, 'inventory/signup.html', {'form': form})
 
 class AddItem(LoginRequiredMixin, CreateView):
-	model = InventoryItem
-	form_class = InventoryItemForm
-	template_name = 'inventory/item_form.html'
-	success_url = reverse_lazy('dashboard')
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['categories'] = Category.objects.all()
-		return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
-	def form_valid(self, form):
-		form.instance.user = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class EditItem(LoginRequiredMixin, UpdateView):
-	model = InventoryItem
-	form_class = InventoryItemForm
-	template_name = 'inventory/item_form.html'
-	success_url = reverse_lazy('dashboard')
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
 
 class DeleteItem(LoginRequiredMixin, DeleteView):
-	model = InventoryItem
-	template_name = 'inventory/delete_item.html'
-	success_url = reverse_lazy('dashboard')
-	context_object_name = 'item'
+    model = InventoryItem
+    template_name = 'inventory/delete_item.html'
+    success_url = reverse_lazy('dashboard')
+    context_object_name = 'item'
+
+class SearchSuggestions(View):
+    def get(self, request):
+        query = request.GET.get("q", "")
+        if query:
+            items = InventoryItem.objects.filter(name__icontains=query, user=request.user).values("name")[:5]
+            return JsonResponse(list(items), safe=False)
+        return JsonResponse([], safe=False)
