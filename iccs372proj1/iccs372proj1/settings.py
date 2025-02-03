41e5f513e511c4695d9d50256b412b147806c70b
sumit0412 <sumit.sachdev412@gmail.com>
2025-02-03 21:47:36 +0700

    Revert "final commit before deploying"
    This reverts commit 5f1770723af6fb5884997a86873fa33ef4b9a7a8.

--- iccs372proj1/iccs372proj1/settings.py
+++ iccs372proj1/iccs372proj1/settings.py
@@ -25,10 +25,10 @@ with open(os.path.join(BASE_DIR, 'project-1-soft-en.json')) as f:
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "xhCKMfgo71Ld5cPgyVXcg31d-H-Ewrf2cT5I3G1GNxKTYydAo63dN5s7drGmjmqDgk8")
+SECRET_KEY = 'django-insecure-+c^g9c+*)q!fs*dll$2*apqk=g@c*6)9%ikw^8t!v)3k+q8fa2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
@@ -131,8 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
